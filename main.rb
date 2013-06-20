5e265881b4f9f66bb3fc4c76f4010da9425b6b7a
Andrea Kennedy <asrkennedy@gmail.com>
2013-06-20 23:49:28 +0100

    unbreakable?'

--- main.rb
+++ main.rb
@@ -27,13 +27,14 @@ end
 
 def menu
   puts `clear`
-  puts "Welcome to the Happitail Animal Shelter!"
-  puts "Type in the number of what action you would you like to do?"
-  puts "1) List animals for adoption'"
+  puts "-------Welcome to the Happitail Animal Shelter!------"
+  puts
+  puts "Type in the number of what you would you like to do?"
+  puts "1) List animals for adoption"
   puts "2) List shelter's client names"
   puts "3) Adopt an animal"
   puts "4) Return an animal"
-  puts "q) quit"
+  puts "Q) quit"
   gets.chomp.downcase
 end
 
@@ -42,33 +43,66 @@ response = menu
 while response != "q"
   case response
   when "1"
-    puts h.available_animals.keys.join(", ")
+    puts "Our pets are #{h.available_animals.keys.join(", ")}."
     puts "Press enter to return to the menu"
   when '2'
-    puts h.clients.keys.join(", ")
+    puts "Our clients are #{h.clients.keys.join(", ")}."
     puts "Press enter to return to the menu"
   when '3'
     puts "Which client are you?"
     puts h.clients.keys.join(", ")
-    client_name = gets.chomp.to_sym
-    puts "What pet would you like to adopt?"
-    puts h.available_animals.keys.join(", ")
-      adopt_pet = gets.chomp.to_sym
-      h.do_adopt (client_name), (adopt_pet)
-    puts "You have now adopted a pet!"
-    puts "Press enter to return to the menu"
-
+    client_name = gets.chomp.downcase.to_sym
+      if h.clients.keys.include? client_name
+            while true
+              if (h.available_animals.keys.size) < 1
+                  puts "No more pets to adopt!"
+                  break
+                end
+              puts "What pet would you like to adopt?"
+              puts h.available_animals.keys.join(", ")
+              adopt_pet = gets.chomp.downcase.to_sym
+                if h.available_animals.keys.include? adopt_pet
+                  h.do_adopt (client_name), (adopt_pet)
+                  puts "You have now adopted a pet!"
+                  puts "Press enter to return to the menu"
+                  break
+               else
+                  puts "You can't adopt a pet that doesn't exist!"
+              end
+            end
+      else
+        puts "Sorry you are not one of our clients! Press enter to return to menu."
+        response
+      end
   when '4'
     puts "Which client are you?"
     puts h.clients.keys.join(", ")
-    client_name = gets.chomp.to_sym
-    puts "What pet would you like to abandon?"
-    puts h.clients[client_name].pets.keys.join(", ")
-      abandon_pet = gets.chomp.to_sym
-      h.receive_abandon (client_name.to_sym), (abandon_pet)
-    puts "You have now abandoned a pet :("
-      puts "Press enter to return to the menu"
+    client_name = gets.chomp.downcase.to_sym
+    if h.clients.keys.include? client_name
+      if h.clients[client_name].pets.keys.empty?
+        puts "You have no pets to abandon! Press enter to return to menu."
+        response
+      else
+        while true
+            puts "What pet would you like to abandon?"
+            puts h.clients[client_name].pets.keys.join(", ")
+              abandon_pet = gets.chomp.downcase.to_sym
+              if h.clients[client_name].pets.keys.include? abandon_pet
+                  h.receive_abandon (client_name.to_sym), (abandon_pet)
+                  puts "You have now abandoned a pet :("
+                  puts "Press enter to return to the menu"
+                  break
+              else
+                puts "You can't abandon a pet that you don't have!"
+              end
+        end
+      end
+    else puts "Sorry you are not one of our clients! Press enter to return to menu."
+    end
+ else
+    puts 'Please enter one of the selections.'
   end
+
 
   gets
   response = menu
